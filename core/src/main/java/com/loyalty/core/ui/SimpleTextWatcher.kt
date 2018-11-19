9f3947f317fc42f395fb94e19260fa0400bd4168
Andrei Malaev <andrey1391@mail.ru>
2018-11-19 15:28:16 +0300

    Add SimpleTextWatcher

---
+++ core/src/main/java/com/loyalty/core/ui/SimpleTextWatcher.kt
@@ -0,0 +1,8 @@
+package com.loyalty.core.ui
+
+import android.text.TextWatcher
+
+interface SimpleTextWatcher : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+}
