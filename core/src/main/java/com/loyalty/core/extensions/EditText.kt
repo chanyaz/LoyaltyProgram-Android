0eb3eb3c2632efbb19820e124164bbba3d0388b2
Andrei Malaev <andrey1391@mail.ru>
2018-11-19 15:28:35 +0300

    Add EditText extensions

---
+++ core/src/main/java/com/loyalty/core/extensions/EditText.kt
@@ -0,0 +1,13 @@
+package com.loyalty.core.extensions
+
+import android.text.Editable
+import android.widget.EditText
+import com.loyalty.core.ui.SimpleTextWatcher
+
+fun EditText.setOnTextChangedListener(onTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : SimpleTextWatcher {
+        override fun afterTextChanged(s: Editable) {
+            onTextChanged.invoke(s.toString())
+        }
+    })
+}
