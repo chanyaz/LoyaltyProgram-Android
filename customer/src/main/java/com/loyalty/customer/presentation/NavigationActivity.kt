318a0291eef75d191793c57f2d9ce5bcf17e67b3
Andrei Malaev <andrey1391@mail.ru>
2018-11-19 17:19:12 +0300

    Add navigation activity wip

--- customer/src/main/java/com/loyalty/customer/presentation/NavigationActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.loyalty.customer.presentation
-
-class NavigationActivity {
-}
---
+++ customer/src/main/java/com/loyalty/customer/presentation/navigation/NavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.loyalty.customer.presentation.navigation
+
+import com.loyalty.core.presentation.view.BaseActivity
+
+class NavigationActivity : BaseActivity<Nothing, Nothing, Nothing>() {
+
+    override val layout: Int = 1488
+
+    override val viewModel: Nothing = throw RuntimeException("teeest")
+
+
+}
