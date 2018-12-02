152dca4e15db1f36312b37142e8e38630e2b9205
Andrei Malaev <andrey1391@mail.ru>
2018-12-02 14:59:01 +0300

    Add venue model

---
+++ customer/src/main/java/com/loyalty/customer/ui/models/VenueUIModel.kt
@@ -0,0 +1,8 @@
+package com.loyalty.customer.ui.models
+
+data class VenueUIModel(
+        val imageUrl: String,
+        val name: String,
+        val address: String,
+        val workingHours: String
+)
