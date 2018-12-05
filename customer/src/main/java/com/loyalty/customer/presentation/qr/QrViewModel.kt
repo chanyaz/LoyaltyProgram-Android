8a95b94bd1ec8fbe8ba97ed2d509fbd256659aeb
Andrei Malaev <andrey1391@mail.ru>
2018-12-05 20:05:17 +0300

    Implicitly call on specific thread

--- customer/src/main/java/com/loyalty/customer/presentation/qr/QrViewModel.kt
+++ customer/src/main/java/com/loyalty/customer/presentation/qr/QrViewModel.kt
@@ -2,6 +2,7 @@ package com.loyalty.customer.presentation.qr
 
 import com.loyalty.core.presentation.base.BaseEvent
 import com.loyalty.core.presentation.mvvm.BaseViewModel
+import com.loyalty.core.util.extensions.observeOnUi
 import com.loyalty.customer.usecases.qr.LoadQrBitmapCase
 
 class QrViewModel(
@@ -15,6 +16,7 @@ class QrViewModel(
 
     private fun loadQrCode(qrWidth: Int, qrHeight: Int) {
         subscribe(loadQrBitmapCase.loadQrBitmap(width = qrWidth, height = qrHeight)
+                .observeOnUi()
                 .subscribe({
                     setState(QrState.QrLoaded(it))
                 }, {
