179f0643cbd48384e06bdfaecb82acc2ce202845
Andrei Malaev <andrey1391@mail.ru>
2018-12-08 14:48:52 +0300

    Add venue fragment and corresponding classes WIP

---
+++ customer/src/main/java/com/loyalty/customer/presentation/venue/VenueFragment.kt
@@ -0,0 +1,21 @@
+package com.loyalty.customer.presentation.venue
+
+import com.loyalty.core.presentation.base.BaseEvent
+import com.loyalty.core.presentation.base.BaseState
+import com.loyalty.core.presentation.mvvm.MvvmFragment
+import com.loyalty.customer.R
+import org.koin.android.ext.android.inject
+
+class VenueFragment : MvvmFragment<BaseState, BaseEvent>() {
+
+    override val layout: Int get() =  R.layout.venue_fragment
+
+    override val viewModel: VenueViewModel by inject()
+
+
+
+    companion object {
+        fun newInstance(): VenueFragment = VenueFragment()
+    }
+
+}
---
+++ customer/src/main/java/com/loyalty/customer/presentation/venue/VenueViewModel.kt
@@ -0,0 +1,11 @@
+package com.loyalty.customer.presentation.venue
+
+import com.loyalty.core.presentation.base.BaseEvent
+import com.loyalty.core.presentation.base.BaseState
+import com.loyalty.core.presentation.mvvm.BaseViewModel
+
+class VenueViewModel : BaseViewModel<BaseState, BaseEvent>() {
+
+
+
+}
