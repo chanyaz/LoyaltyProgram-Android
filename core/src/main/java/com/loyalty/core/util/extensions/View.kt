c591a85d7f7b1ded2607b05304242179d4cefcd6
Andrei Malaev <andrey1391@mail.ru>
2018-12-16 17:15:16 +0300

    Add view groups extensions

--- core/src/main/java/com/loyalty/core/util/extensions/View.kt
+++ core/src/main/java/com/loyalty/core/util/extensions/View.kt
@@ -13,3 +13,20 @@ fun View.visible() {
 fun View.invisible() {
     this.visibility = View.INVISIBLE
 }
+
+/* *
+ * Extensions for groups of views are a workaround as
+ * currently constraint layout groups do not properly work
+ * */
+
+fun List<View>.gone() {
+    this.forEach { it.gone() }
+}
+
+fun List<View>.visible() {
+    this.forEach { it.visible() }
+}
+
+fun List<View>.invisible() {
+    this.forEach { it.invisible() }
+}
