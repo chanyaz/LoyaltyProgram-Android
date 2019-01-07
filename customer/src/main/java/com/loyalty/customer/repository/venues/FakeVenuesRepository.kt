e19cab6107d5170990e369c138a2f41c658f2f96
Andrei Malaev <andrey1391@mail.ru>
2019-01-07 17:32:01 +0300

    Edit venues mocks

--- customer/src/main/java/com/loyalty/customer/repository/venues/FakeVenuesRepository.kt
+++ customer/src/main/java/com/loyalty/customer/repository/venues/FakeVenuesRepository.kt
@@ -8,12 +8,12 @@ import java.util.concurrent.TimeUnit
 class FakeVenuesRepository : VenuesRepository {
 
     private val venues: List<VenueItemUIModel> = listOf(
-            VenueItemUIModel("https://img.grouponcdn.com/deal/usyc5qMhACid4zb7b34DdtkC4Vh/us-1374x824/v1/t440x300.jpg", "Calabria", "ул. Чачи, 57", "с 11.00 до 22.00"),
-            VenueItemUIModel("https://img.grouponcdn.com/deal/usyc5qMhACid4zb7b34DdtkC4Vh/us-1374x824/v1/t440x300.jpg", "Coffeeholic", "ул. Чачи, 23", "с 11.00 до 22.00"),
-            VenueItemUIModel("https://img.grouponcdn.com/deal/usyc5qMhACid4zb7b34DdtkC4Vh/us-1374x824/v1/t440x300.jpg", "Zerno", "ул. Чачи, 34", "с 11.00 до 22.00"),
-            VenueItemUIModel("https://img.grouponcdn.com/deal/usyc5qMhACid4zb7b34DdtkC4Vh/us-1374x824/v1/t440x300.jpg", "Expresso", "ул. Чачи, 666", "с 11.00 до 22.00"),
-            VenueItemUIModel("https://img.grouponcdn.com/deal/usyc5qMhACid4zb7b34DdtkC4Vh/us-1374x824/v1/t440x300.jpg", "Lolo4ka", "ул. Чачи, 45", "с 11.00 до 22.00"),
-            VenueItemUIModel("https://img.grouponcdn.com/deal/usyc5qMhACid4zb7b34DdtkC4Vh/us-1374x824/v1/t440x300.jpg", "Wine ", "ул. Чачи, 77", "с 11.00 до 22.00")
+            VenueItemUIModel("https://pbs.twimg.com/profile_images/3680186256/54047b12750379df67270f27d6f4faf0.jpeg", "Zerno", "Минск, пр-т Независимости, 46", "с 07.00 до 23.00"),
+            VenueItemUIModel("https://comelite-arch.com/wp-content/uploads/2018/04/student-coffee-shop-design-8-256x256.jpg", "Coffeeholic", "Минск, ул. Якуба Коласа, 33", "с 08.00 до 22.00"),
+            VenueItemUIModel("https://cdn131.picsart.com/282910112020201.jpg?c256x256", "Wake Up Coffee", "Минск, ул. В. Хоружей, 5", "с 08.00 до 23.00"),
+            VenueItemUIModel("https://pbs.twimg.com/profile_images/3216825928/450dc141f7e16a3677bd186c14929ff5.jpeg", "Surf Coffee", "Минск, ул. Кирова, 19", "с 08.00 до 23.00"),
+            VenueItemUIModel("https://barcentral.com.au/wp-content/uploads/bar-256x256.jpg", "La coffee", "Минск, ул. Козлова, 5", "с 08.00 до 23.00"),
+            VenueItemUIModel("https://barcentral.com.au/wp-content/uploads/Imterior-shot-256x256.jpg", "CoffeeBerry", "Минск, ул. Интернациональная, 5", "с 08.00 до 23.00")
     )
 
     override fun getVenues(city: String): Single<List<VenueItemUIModel>> {
