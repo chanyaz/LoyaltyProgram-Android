9f11c69e8b5ce60536300b29e325a6d5d443b3b2
Andrei Malaev <andrey1391@mail.ru>
2019-01-08 20:03:22 +0300

    Add card rendering

--- customer/src/main/java/com/loyalty/customer/presentation/cards/adapter/holders/CardViewHolder.kt
+++ customer/src/main/java/com/loyalty/customer/presentation/cards/adapter/holders/CardViewHolder.kt
@@ -1,9 +1,11 @@
 package com.loyalty.customer.presentation.cards.adapter.holders
 
 import android.annotation.SuppressLint
+import android.support.v4.content.ContextCompat
 import android.view.View
 import com.bumptech.glide.Glide
 import com.loyalty.core.ui.adapter.SimpleDelegationHolder
+import com.loyalty.customer.R
 import com.loyalty.customer.ui.models.card.CardItemUIModel
 import kotlinx.android.synthetic.main.card_item_collapsed.view.cardDate
 import kotlinx.android.synthetic.main.card_item_collapsed.view.cardDaysLeft
@@ -34,10 +36,28 @@ class CardViewHolder(itemView: View) : SimpleDelegationHolder<CardItemUIModel>(i
             cardDate.text = eventDate
             cardNumberOfStamps.text = currentStamps.toString()
             cardNumberOutOf.text = "/$outOfStamps"
+
+            val quantityColor = ContextCompat.getColor(
+                    cardNumberOfStamps.context,
+                    if (currentStamps >= outOfStamps) R.color.cards_points_exceed_color else R.color.cards_points_color
+            )
+
+            cardNumberOfStamps.setTextColor(quantityColor)
+            cardNumberOutOf.setTextColor(quantityColor)
+
             Glide.with(itemView).load(imageUrl).into(cardLogo)
 
             if (isExpandedState) {
-                /* todo */
+                val filledPercentage = currentStamps / outOfStamps.toDouble()
+
+                when {
+                    filledPercentage == 0.0 -> cardTypeView.setImageResource(R.drawable.img_coffe_cap_0)
+                    filledPercentage < 0.3 -> cardTypeView.setImageResource(R.drawable.img_coffe_cap_20)
+                    filledPercentage < 0.5 -> cardTypeView.setImageResource(R.drawable.img_coffe_cap_40)
+                    filledPercentage < 0.7 -> cardTypeView.setImageResource(R.drawable.img_coffe_cap_60)
+                    filledPercentage < 0.9 -> cardTypeView.setImageResource(R.drawable.img_coffe_cap_80)
+                    else -> cardTypeView.setImageResource(R.drawable.img_coffe_cap_100)
+                }
             }
         }
     }
