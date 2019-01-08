c415cc1ff03299fbb10cd52e35133612a98f48a0
Andrei Malaev <andrey1391@mail.ru>
2019-01-08 21:44:44 +0300

    Change some delays

--- customer/src/main/java/com/loyalty/customer/usecases/venue/LoadVenuePageImpl.kt
+++ customer/src/main/java/com/loyalty/customer/usecases/venue/LoadVenuePageImpl.kt
@@ -3,6 +3,8 @@ package com.loyalty.customer.usecases.venue
 import com.loyalty.customer.repository.venuepage.VenuePageRepository
 import com.loyalty.customer.ui.models.venue.VenuePageUIModel
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class LoadVenuePageImpl(
         private val venuePageRepository: VenuePageRepository
@@ -10,5 +12,6 @@ class LoadVenuePageImpl(
 
     override fun invoke(id: String): Single<VenuePageUIModel> =
             venuePageRepository.loadVenuePage("lol")
+                    .delay(500, TimeUnit.MILLISECONDS, Schedulers.io())
 
 }
