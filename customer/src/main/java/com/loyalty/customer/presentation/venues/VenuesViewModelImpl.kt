5f30904d24a9779056b73d6bc78cb5babada996a
Andrei Malaev <andrey1391@mail.ru>
2019-01-17 22:32:05 +0300

    Add todo

--- customer/src/main/java/com/loyalty/customer/presentation/venues/VenuesViewModelImpl.kt
+++ customer/src/main/java/com/loyalty/customer/presentation/venues/VenuesViewModelImpl.kt
@@ -52,7 +52,7 @@ class VenuesViewModelImpl(
         )
     }
 
-    override fun selectVenue(position: Int) {
+    override fun selectVenue(position: Int) { /* todo need position of the filtered venue!!!!*/
         router.hideNAdd(CustomerScreens.KEY_VENUE_NEW.name)
     }
 
